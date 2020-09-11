da96ecfa0a741aa7f128bdfade71145a8fd1a4f6
Tan.Donats <70358918+tndonats@users.noreply.github.com>
2020-09-11 21:39:52 +0700

    Add files via upload

---
+++ HackGameByTn.Donats.sh
@@ -0,0 +1,25 @@
+#1/bin/bash
+#TN.DONATS SECURITY
+clear
+figlet Tn.Donats | loncat
+echo "______________________________________"
+echo "Author : Tn. Donats"
+echo 
+echo "{1}Hack Game By;Tn.Donats"
+echo 
+read -p {Pilih Nomor}>>> pill
+#batas boss
+if { $pill = "1" }
+then
+echo "Sedang Loading.... " sleep 2 
+gitu clone https://github.com/HackerGamers71/Hacking-Toolkit
+cd Hacking-Toolkit
+sh TAMFAN. sh
+echo "Loading Selesai..... "
+fi
+#batas bos
+if { $pill = {0} }
+then
+echo "Terimakasih...."
+exit
+fi
